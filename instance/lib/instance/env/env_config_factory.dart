a66cd6b0bd5f54901d67acf8a41c87e497fa5e64
vinihzets <viniciusdaarky@outlook.com>
2025-09-12 20:03:37 -0300

    Remoção de main_dev.dart

--- instance/lib/instance/env/env_config_factory.dart
+++ instance/lib/instance/env/env_config_factory.dart
@@ -3,7 +3,4 @@ import 'package:core/app_way_data_core.dart';
 class EnvConfigFactory {
   static EnvConfig get prd =>
       EnvConfig(baseUrl: 'https://rickandmortyapi.com/api', env: Env.prd);
-
-  static EnvConfig get dev =>
-      EnvConfig(baseUrl: 'https://rickandmortyapi.com/api', env: Env.dev);
 }
